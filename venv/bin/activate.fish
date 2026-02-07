d43b9adeb1a37841f8957a7ee793ab54ad92b353
meeraskurup <meeraskurup19@gmail.com>
2026-02-06 19:25:21 -0800

    more pushes
    small updates

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/meerakurup/Downloads/cursor-ai/ai-news-aggregator/venv"
+set -gx VIRTUAL_ENV "/Users/meerakurup/Downloads/Agents/ai-news-aggregator/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
